193ae31e446b54a56e7963215ea7a2b2023ae74a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-19 22:26:05 -0400

    to db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,12 +24,69 @@ namespace assignment_chefs_n_dishes.Controllers
 
     public IActionResult Index()
     {
+      return RedirectToAction("Chefs");
+    }
+
+    [HttpGet("/chefs")]
+    public IActionResult Chefs()
+    {
+      return View();
+    }
+    [HttpGet("/dishes")]
+    public IActionResult Dishes()
+    {
+      return View();
+    }
+    [HttpGet("/chefs/new")]
+    public IActionResult NewChef()
+    {
+      return View();
+    }
+    [HttpGet("/dishes/new")]
+    public IActionResult NewDish()
+    {
       return View();
     }
 
-    public IActionResult Privacy()
+    /////////////////
+
+    [HttpPost("chefs/create")]
+    public IActionResult AddChef(Chef newChef)
     {
-      return View();
+      if (ModelState.IsValid)
+      {
+        _context.Chefs.Add(newChef);
+        _context.SaveChanges();
+
+        ViewBag.AllChefs = _context.Chefs.OrderBy(a => a.FirstName).ToList();
+        ViewBag.AllDishes = _context.Dishes.OrderBy(a => a.Name).ToList();
+        return RedirectToAction("Chefs");
+      }
+      else
+      {
+        ViewBag.AllChefs = _context.Chefs.OrderBy(a => a.FirstName).ToList();
+        ViewBag.AllDishes = _context.Dishes.OrderBy(a => a.Name).ToList();
+        return View("NewChef");
+      }
+    }
+    [HttpPost("dishes/create")]
+    public IActionResult AddDish(Dish newDish)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Dishes.Add(newDish);
+        _context.SaveChanges();
+
+        ViewBag.AllChefs = _context.Chefs.OrderBy(a => a.FirstName).ToList();
+        ViewBag.AllDishes = _context.Dishes.OrderBy(a => a.Name).ToList();
+        return RedirectToAction("Dishes");
+      }
+      else
+      {
+        ViewBag.AllChefs = _context.Chefs.OrderBy(a => a.FirstName).ToList();
+        ViewBag.AllDishes = _context.Dishes.OrderBy(a => a.Name).ToList();
+        return View("NewDish");
+      }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
